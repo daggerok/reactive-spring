251d02bc778bac457c58f0dde9ebbadcd4ce6e07
Maksim Kostromin <daggerok@gmail.com>
2016-03-19 23:05:17 +0200

    rx extension

--- src/main/java/com/daggerok/spring/reactive/cfg/RxCfg.java
+++ src/main/java/com/daggerok/spring/reactive/cfg/RxCfg.java
@@ -5,6 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import rx.Observable;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.lang.System.out;
 import static java.util.Arrays.asList;
@@ -14,6 +18,10 @@ import static java.util.Arrays.asList;
 public class RxCfg {
     @Bean
     public CommandLineRunner runner() {
+        Observable.just(IntStream.range(0, 3)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toSet()))
+                .take(2).subscribe(out::println);
         return args -> reactor.rx.Streams
                 .from(asList("one, two, three".split(", ")))
                 .log()
